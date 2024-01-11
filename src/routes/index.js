1a379318806ac0784f1053f890c231f28f89a326
ThanhPhuong-Dev <thanhphuong212002@gmail.com>
2024-01-11 22:40:49 +0700

    Crud-Product

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
-const userRouter = require('./user');
+const userRouter = require('./userRouter');
+const productRouter = require('./productRouter');
 
 const routes = (app) => {
   app.use('/user', userRouter);
+  app.use('/product', productRouter);
 };
 
 module.exports = routes;
